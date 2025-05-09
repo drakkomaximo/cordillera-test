1f92b9405bda091fe25bea41ca8e9df2d00a9530
drakkomaximo <andres.villanueva.tabares@gmail.com>
2025-05-08 19:28:21 -0500

    Footer has been hidden

--- src/components/layout/Logos.tsx
+++ src/components/layout/Logos.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Logos() {
   return (
-    <div className=" py-[33px] md:py-[120px]">
+    <div className="hidden py-[33px] md:py-[120px]">
       <Image className='w-full md:hidden' src='/log_mobile.png' alt='logos' width={375} height={100} />
       <Image className='w-full hidden md:block max-w-[1080px] mx-auto' src='/log_desktop.png' alt='logos' width={1080} height={100} />
     </div>
