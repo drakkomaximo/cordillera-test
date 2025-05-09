195efe75ad92326f5663d8318f59fae7bf2802ea
drakkomaximo <andres.villanueva.tabares@gmail.com>
2025-05-08 19:45:22 -0500

    Footer has been added

--- src/components/layout/Logos.tsx
+++ src/components/layout/Logos.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Logos() {
   return (
-    <div className="hidden py-[33px] md:py-[120px]">
-      <Image className='w-full md:hidden' src='/log_mobile.png' alt='logos' width={375} height={100} />
-      <Image className='w-full hidden md:block max-w-[1080px] mx-auto' src='/log_desktop.png' alt='logos' width={1080} height={100} />
+    <div className="py-[33px] md:py-[120px]">
+      <Image className='w-full md:hidden' src='/footer-sponsors-mobile.png' alt='footer-sponsors-mobile' width={375} height={100} />
+      <Image className='w-full hidden md:block max-w-[1080px] mx-auto' src='/footer-sponsors-desktop.png' alt='footer-sponsors-desktop' width={1080} height={100} />
     </div>
   );
 }
